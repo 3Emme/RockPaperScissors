b578f40b6ab9d2ea71de74e3f6b53434739f2e24
3Emme <emmettbuentiempo@gmail.com>
2020-09-23 15:25:29 -0700

    write and pass test 2 and 3.
    co-authored-by: Natalie Murphy <NatalieMurphy500@gmail.com>

--- RockPaperScissors.Tests/ModelTests/RockPaperScissorsTests.cs
+++ RockPaperScissors.Tests/ModelTests/RockPaperScissorsTests.cs
@@ -15,16 +15,32 @@ namespace RockPaperScissors.Tests
       Assert.AreEqual(typeof(Game), newGame.GetType());
     }
     [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
+    public void GamePlay_ReturnsPlayGameResult_String()
     {
       //Arrange
-  
-
+      string input = "Rock";
+      string input2 = "Scissors";
+      Game newGame = new Game(input, input2);
       //Act
-  
+      string result = newGame.PlayGame();
 
       //Assert
-  
+      Assert.AreEqual(("I don't know what just happened?"), result);
+
+    }
+    [TestMethod]
+    public void GamePlay_ReturnsPlayTie_String()
+    {
+      //Arrange
+      string input = "Scissors";
+      string input2 = "Scissors";
+      Game newGame = new Game(input, input2);
+      //Act
+      string result = newGame.PlayGame();
+
+      //Assert
+      Assert.AreEqual(("It's a Tie!"), result);
+
     }
   }
 }
